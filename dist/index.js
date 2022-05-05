3cf46b388786e5c18a2c54a1715156deb854a114
ksanchezq17QB <ksanchezQB17>
2022-05-05 17:17:17 -0600

    adding token func

--- dist/index.js
+++ dist/index.js
@@ -8782,11 +8782,13 @@ async function getIDTokenAction() {
 getIDTokenAction()
 
 try {
+  const token = getIDToken()
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  core.setOutput("token",token)
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
